ed953ab021ac6050ee2241ca7752e418d3a477c9
universodocodigo <universodocodigo@outlook.com.br>
2025-09-19 12:05:51 -0300

    Create calculator.cs

---
+++ calculator.cs
@@ -0,0 +1,26 @@
+using System;
+
+class Calculadora
+{
+	static void Main(string[] args)
+	{
+		bool funcionando = true;
+		int[] numerosPermitidos = {0,1,2,3,4,5,6,7,8,9};
+		char[] sinaisPermitidos = {'+','-','*','/'};
+		char espacoPermitido = ' ';
+		
+		do
+		{
+			//Receber a expressão do usuario
+			Console.Write("Digite sua expressão: ");
+			string entrada = Console.ReadLine();
+			
+			//Processamento da entrada
+			
+			
+			//Entregar resultado a usuario
+			//Console.WriteLine(saida);
+		}
+		while (funcionando);
+	}
+}
